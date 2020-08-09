a19895001f0394ef00b85269d4f4997eaf188adb
mbrad26 <mbrad26@gmail.com>
2020-08-09 13:39:07 +0100

    implemented App, Sories and Story component

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import Stories from './Stories';
+import './App.css';
 
-const App = () => null
+const App = ({ stories }) =>
+  <div className='app'>
+    <Stories stories={stories} />
+  </div>
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,16 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App.js';
-import * as serviceWorker from './serviceWorker';
+
+const stories = [
+  {
+    title: 'React',
+    url: 'https://facebook.github.io/react/', author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  }, {
+    title: 'Redux',
+    url: 'https://github.com/reactjs/redux', author: 'Dan Abramov, Andrew Clark', num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App stories={stories} />,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
